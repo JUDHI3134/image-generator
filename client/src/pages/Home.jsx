e00030da48c034c40cbda8a9c988a3c1e220a376
JUDHI3134 <judhistirbehera241@gmail.com>
2024-11-19 14:36:07 +0530

    create description section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
 import Steps from '../components/Steps'
+import Desccription from '../components/Desccription'
 
 const Home = () => {
   return (
     <div>
       <Header />
       <Steps />
+      <Desccription />
     </div>
   )
 }
