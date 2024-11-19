d0673ee0fb4344fbf00ac9e767186e3681d0e24a
JUDHI3134 <judhistirbehera241@gmail.com>
2024-11-19 15:10:21 +0530

    create generate button

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Header from '../components/Header'
 import Steps from '../components/Steps'
 import Desccription from '../components/Desccription'
 import Testimonial from '../components/Testimonial'
+import GenerateBtn from '../components/GenerateBtn'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <Steps />
       <Desccription />
       <Testimonial />
+      <GenerateBtn />
     </div>
   )
 }
