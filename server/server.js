2c94972b871219e508e089634cef614baccd41cc
JUDHI3134 <judhistirbehera241@gmail.com>
2024-11-20 19:54:19 +0530

    create register login and user credits endpoint

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import "dotenv/config"
 import connectDB from "./config/mongodb.js";
+import userRouter from "./routes/userRoute.js";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -13,6 +14,8 @@ await connectDB();
 app.get("/",(req,res)=>{
     res.send("Api Working")
 })
+
+app.use("/api/user",userRouter)
 
 app.listen(PORT,()=>{
     console.log(`server run port ${PORT}`);
